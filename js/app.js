c23d9bac52acbb0bd62da0dbb5b19cfce602a77b
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-12-01 09:10:37 -0600

    fixed issue

--- js/app.js
+++ js/app.js
@@ -1,159 +1,160 @@
-var data = [
-    ["", "Ford", "Volvo", "Toyota", "Honda"],
-    ["2016", 10, 11, 12, 13],
-    ["2017", 20, 11, 14, 13],
-    ["2018", 30, 15, 12, 13]
-];
-var years = [];
-var ford = [];
-var volvo = [];
-var toyota = [];
-var honda = [];
+(function() {
+
+    var data = [
+        ["", "Ford", "Volvo", "Toyota", "Honda"],
+        ["2016", 10, 11, 12, 13],
+        ["2017", 20, 11, 14, 13],
+        ["2018", 30, 15, 12, 13]
+    ];
+    var years = [];
+    var ford = [];
+    var volvo = [];
+    var toyota = [];
+    var honda = [];
 
 
-function refreshChart(changes, source) {
-    years = []
-    ford= [];
-    volvo= [];
-    toyota= [];
-    honda = [];
+    function refreshChart(changes, source) {
+        years = []
+        ford = [];
+        volvo = [];
+        toyota = [];
+        honda = [];
 
-    for (var i = 1; i < data.length; i++) {
-        years.push(data[i][0])
-    }
-    for (var i = 1; i < data.length; i++) {
-        ford.push(parseInt(data[i][1]))
-    }
-    for (var i = 1; i < data.length; i++) {
-        volvo.push(parseInt(data[i][2]))
-    }
-    for (var i = 1; i < data.length; i++) {
-        toyota.push(parseInt(data[i][3]))
-    }
-    for (var i = 1; i < data.length; i++) {
-        honda.push(parseInt(data[i][4]))
-    }
-    if(hot){
-      myChart.config.data.datasets[0].data = ford;
-      myChart.config.data.datasets[1].data = volvo;
-      myChart.config.data.datasets[2].data = toyota;
-      myChart.config.data.datasets[3].data = honda;
-      myChart.update();
+        for (var i = 1; i < data.length; i++) {
+            years.push(data[i][0])
+        }
+        for (var i = 1; i < data.length; i++) {
+            ford.push(parseInt(data[i][1]))
+        }
+        for (var i = 1; i < data.length; i++) {
+            volvo.push(parseInt(data[i][2]))
+        }
+        for (var i = 1; i < data.length; i++) {
+            toyota.push(parseInt(data[i][3]))
+        }
+        for (var i = 1; i < data.length; i++) {
+            honda.push(parseInt(data[i][4]))
+        }
+        if (hot) {
+            myChart.config.data.datasets[0].data = ford;
+            myChart.config.data.datasets[1].data = volvo;
+            myChart.config.data.datasets[2].data = toyota;
+            myChart.config.data.datasets[3].data = honda;
+            myChart.update();
 
-    }
-}
-
-var container = document.getElementById('example');
-var hot = new Handsontable(container, {
-    data: data,
-    rowHeaders: true,
-    colHeaders: true,
-    afterChange: refreshChart
-
-});
-
-var render = (function(){
-})
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: years,
-
-        datasets: [{
-            label: '# of Ford',
-            data: ford,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }, {
-            label: '# of volvo',
-            data: volvo,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }, {
-            label: '# of Toyota',
-            data: toyota,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }, {
-            label: '# of Honda',
-            data: honda,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
         }
     }
-});
 
+    var container = document.getElementById('example');
+    var hot = new Handsontable(container, {
+        data: data,
+        rowHeaders: true,
+        colHeaders: true,
+        afterChange: refreshChart
 
+    });
+
+    var render = (function() {})
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: years,
+
+            datasets: [{
+                label: '# of Ford',
+                data: ford,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of volvo',
+                data: volvo,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of Toyota',
+                data: toyota,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of Honda',
+                data: honda,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+})()
 
 
 
